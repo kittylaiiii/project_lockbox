b494a7a518bd427ef01a6a08dcc0d34286e1f5ca
Kitty Lai <kittylai@mac.milton.edu>
2025-03-07 08:42:26 -0500

    x

--- project_lockbox.ino
+++ project_lockbox.ino
@@ -1,3 +1,8 @@
+
+
+//DO NOT WIRE THIS WITH 5V --> 3.3
+//dont wire IRQ
+//rst 9, miso 12, mosi 11, sca 13, sda 10
 #include <MFRC522.h>
 
 #include <MFRC522Extended.h>
@@ -5,6 +10,102 @@
 #include <deprecated.h>
 
 #include <require_cpp11.h>
+
+
+
+/*
+
+ * --------------------------------------------------------------------------------------------------------------------
+
+ * Example sketch/program showing how to read data from a PICC to serial.
+
+ * --------------------------------------------------------------------------------------------------------------------
+
+ * This is a MFRC522 library example; for further details and other examples see: https://github.com/miguelbalboa/rfid
+
+ * 
+
+ * Example sketch/program showing how to read data from a PICC (that is: a RFID Tag or Card) using a MFRC522 based RFID
+
+ * Reader on the Arduino SPI interface.
+
+ * 
+
+ * When the Arduino and the MFRC522 module are connected (see the pin layout below), load this sketch into Arduino IDE
+
+ * then verify/compile and upload it. To see the output: use Tools, Serial Monitor of the IDE (hit Ctrl+Shft+M). When
+
+ * you present a PICC (that is: a RFID Tag or Card) at reading distance of the MFRC522 Reader/PCD, the serial output
+
+ * will show the ID/UID, type and any data blocks it can read. Note: you may see "Timeout in communication" messages
+
+ * when removing the PICC from reading distance too early.
+
+ * 
+
+ * If your reader supports it, this sketch/program will read all the PICCs presented (that is: multiple tag reading).
+
+ * So if you stack two or more PICCs on top of each other and present them to the reader, it will first output all
+
+ * details of the first and then the next PICC. Note that this may take some time as all data blocks are dumped, so
+
+ * keep the PICCs at reading distance until complete.
+
+ * 
+
+ * @license Released into the public domain.
+
+ * 
+
+ * Typical pin layout used:
+
+ * -----------------------------------------------------------------------------------------
+
+ * MFRC522 Arduino Arduino Arduino Arduino Arduino
+
+ * Reader/PCD Uno/101 Mega Nano v3 Leonardo/Micro Pro Micro
+
+ * Signal Pin Pin Pin Pin Pin Pin
+
+ * -----------------------------------------------------------------------------------------
+
+ * RST/Reset RST 9 5 D9 RESET/ICSP-5 RST
+
+ * SPI SS SDA(SS) 10 53 D10 10 10
+
+ * SPI MOSI MOSI 11 / ICSP-4 51 D11 ICSP-4 16
+
+ * SPI MISO MISO 12 / ICSP-1 50 D12 ICSP-1 14
+
+ * SPI SCK SCK 13 / ICSP-3 52 D13 ICSP-3 15
+
+ *
+
+ * More pin layouts for other boards can be found here: https://github.com/miguelbalboa/rfid#pin-layout
+
+ */
+
+//zip
+
+//step 1: dowlnoad zip (do not unzip) https://github.com/miguelbalboa/rfid
+
+//step 2: sketch -> include libraries -> add zip
+
+//setp 3: find the one we just downloaded and say open
+
+
+
+//dump file
+
+//flie -> examples -> mfrc522 -> dumpinfo
+
+
+
+//wire with 3.3 v not 5v because that will fry it
+
+
+
+
 
 #include <SPI.h>
 
@@ -19,9 +120,6 @@
 
 
 MFRC522 mfrc522(SS_PIN, RST_PIN); // Create MFRC522 instance
-
-
-
 
 
 //bluetooth stuff:
@@ -39,14 +137,11 @@ char a ; //an empty character to store
 
 
 //ultrasonic sensor stuff:
-#define TRIGGER_PIN 4 //this is the pin that sends out the pulse
-#define ECHO_PIN 3 //this is the pin that reads the distance
+#define TRIGGER_PIN 7 //this is the pin that sends out the pulse
+#define ECHO_PIN 8 //this is the pin that reads the distance
 
 void setup() {
 
-
-
-//RFID stuff:
 Serial.begin(9600); // Initialize serial communications with the PC
 
 while (!Serial); // Do nothing if no serial port is opened (added for Arduinos based on ATMEGA32U4)
@@ -62,8 +157,6 @@ mfrc522.PCD_DumpVersionToSerial(); // Show details of PCD - MFRC522 Card Reader 
 Serial.println(F("Scan PICC to see UID, SAK, type, and data blocks..."));
 
 
-//bluetooth stuff:
-  Serial.begin(9600); // means can print on serial monitor
   tooth.begin(9600); //can print on device
 
   pinMode(RED_LED, OUTPUT);
@@ -73,19 +166,11 @@ Serial.println(F("Scan PICC to see UID, SAK, type, and data blocks..."));
 //ultrasonic sensor stuff:
   pinMode(TRIGGER_PIN, OUTPUT); //output bc it sends pulse out
   pinMode(ECHO_PIN, INPUT); //bc it is reading what is coming in
-
 }
 
+
+
 void loop() {
-
-
-digitalWrite(BUZZER, HIGH);
-delay(100);
-digitalWrite(BUZZER, LOW);
-delay(100);
-
-
-//RFID stuff :
 
  // Reset the loop if no new card present on the sensor/reader. This saves the entire process when idle.
 
