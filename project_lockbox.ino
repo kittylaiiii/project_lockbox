218b8666019450faecaefc26f2566414a59f5a96
Kitty Lai <kittylai@Kittys-MacBook-Air-5.local>
2025-04-16 13:13:08 -0400

    changed

--- project_lockbox.ino
+++ project_lockbox.ino
@@ -108,188 +108,201 @@
 #include <MFRC522.h>
 
 
-#include <Servo.h> 
-#define SPIN_PIN A0
-Servo spin;//make a servo object called spin
+#include <Servo.h>
+#define SPIN_PIN 2
+Servo spin;  //make a servo object called spin
 
-#define RST_PIN 9 // Configurable, see typical pin layout above
+#define RST_PIN 9  // Configurable, see typical pin layout above
 
-#define SS_PIN 10 // Configurable, see typical pin layout above
+#define SS_PIN 10  // Configurable, see typical pin layout above
 
 
 
-MFRC522 mfrc522(SS_PIN, RST_PIN); // Create MFRC522 instance
+MFRC522 mfrc522(SS_PIN, RST_PIN);  // Create MFRC522 instance
 
 
 //bluetooth stuff:
-int const RX_PIN = 8; //receiving bluetooth
-int const TX_PIN = 7; //transmitting bluetooth
-#define RED_LED 2
+int const RX_PIN = 8;  //receiving bluetooth
+int const TX_PIN = 7;  //transmitting bluetooth
 #define GREEN_LED 5
 #define BUZZER 6
-#define BUTTON A4
 
-#include <SoftwareSerial.h> //include library
 
-SoftwareSerial tooth(TX_PIN, RX_PIN); //create a softwareserial object, set tx and rx pins, tx goes first then rx
+#include <SoftwareSerial.h>  //include library
 
-String input = " " ; //an empty string to store 
+SoftwareSerial tooth(TX_PIN, RX_PIN);  //create a softwareserial object, set tx and rx pins, tx goes first then rx
+
+String input = " ";  //an empty string to store
 
 
 //ultrasonic sensor stuff:
-#define TRIGGER_PIN 4 //this is the pin that sends out the pulse
-#define ECHO_PIN 3 //this is the pin that reads the distance
+#define TRIGGER_PIN 4  //this is the pin that sends out the pulse
+#define ECHO_PIN 3     //this is the pin that reads the distance
 
 void setup() {
 
-Serial.begin(9600); // Initialize serial communications with the PC
+  Serial.begin(9600);  // Initialize serial communications with the PC
 
-while (!Serial); // Do nothing if no serial port is opened (added for Arduinos based on ATMEGA32U4)
+  while (!Serial)
+    ;  // Do nothing if no serial port is opened (added for Arduinos based on ATMEGA32U4)
 
-SPI.begin(); // Init SPI bus
+  SPI.begin();  // Init SPI bus
 
-mfrc522.PCD_Init(); // Init MFRC522
+  mfrc522.PCD_Init();  // Init MFRC522
 
-delay(4); // Optional delay. Some board do need more time after init to be ready, see Readme
+  delay(4);  // Optional delay. Some board do need more time after init to be ready, see Readme
 
-mfrc522.PCD_DumpVersionToSerial(); // Show details of PCD - MFRC522 Card Reader details
+  mfrc522.PCD_DumpVersionToSerial();  // Show details of PCD - MFRC522 Card Reader details
 
-Serial.println(F("Scan PICC to see UID, SAK, type, and data blocks..."));
+  Serial.println(F("Scan PICC to see UID, SAK, type, and data blocks..."));
 
 
-  tooth.begin(9600); //can print on device
+  tooth.begin(9600);  //can print on device
 
-  pinMode(RED_LED, OUTPUT);
+
   pinMode(GREEN_LED, OUTPUT);
   pinMode(BUZZER, OUTPUT);
-  pinMode(BUTTON, INPUT);
 
-//ultrasonic sensor stuff:
-  pinMode(TRIGGER_PIN, OUTPUT); //output bc it sends pulse out
-  pinMode(ECHO_PIN, INPUT); //bc it is reading what is coming in
 
-spin.attach(A0);
+  //ultrasonic sensor stuff:
+  pinMode(TRIGGER_PIN, OUTPUT);  //output bc it sends pulse out
+  pinMode(ECHO_PIN, INPUT);      //bc it is reading what is coming in
 
+  spin.attach(A0);
 
 }
 
 
 
 void loop() {
+  spin.write(60);
+  delay(9000);
+  spin.write(120);
+  delay(9000);
 
-spin.write(60);
-delay(9000);
-spin.write(120);
-delay(9000);
+
+byte authorizedUID[] = { 0x96, 0xDE, 0x29, 0x03 };  // Example UID (change to your real UID)
+byte uidLength = 4;                                 // Length of the UID
+if (mfrc522.uid.size == uidLength) {
+  boolean authorized = true;
+  for (byte i = 0; i < uidLength; i++) {
+    if (mfrc522.uid.uidByte[i] != authorizedUID[i]) {
+      authorized = false;
+      break;
+    }
+  }
+
+  if (authorized) {
+    Serial.println("Access granted.");
+  }else{
+    Serial.println("Wrong RFID. Access not granted.");
+  }
+}
 
 
-//ultrasonic sensor stuff
+
+  //ultrasonic sensor stuff
 
   digitalWrite(TRIGGER_PIN, LOW);
   delayMicroseconds(2);
   digitalWrite(TRIGGER_PIN, HIGH);
-  delayMicroseconds(10); //this is how long it has to wait to send out a pulse
+  delayMicroseconds(10);  //this is how long it has to wait to send out a pulse
 
-  float duration = pulseIn(ECHO_PIN, HIGH); // tell us the time from pulse sent to pulse received
+  float duration = pulseIn(ECHO_PIN, HIGH);  // tell us the time from pulse sent to pulse received
   Serial.println(duration);
 
   //v=s/t
-  float speed = 0.034; //measured in cm/microseconds
-  float distance = (speed * duration)/2;
-  
+  float speed = 0.034;  //measured in cm/microseconds
+  float distance = (speed * duration) / 2;
+
   Serial.println("distance:");
   Serial.println(distance);
   delay(100);
 
-if (distance <= 10){
-  Serial.println("Step back! You're too close!");
-  digitalWrite(BUZZER, HIGH);
-  delay(200);
-  digitalWrite(BUZZER, LOW);
-  delay(200);
-}
-
-tooth.print("Enter your password:");
-
-  if (tooth.available()>0){ //if something is inputed on device
-  input = tooth.read();
-  tooth.print("Password: ");
-  tooth.println(input);
+  if (distance <= 10) {
+    Serial.println("Step back! You're too close!");
+    digitalWrite(BUZZER, HIGH);
+    delay(200);
+    digitalWrite(BUZZER, LOW);
+    delay(200);
   }
 
-if (input != "0329"){
-  digitalWrite(BUZZER, HIGH);
-  delay(1000);
-  digitalWrite(BUZZER, LOW);
-  delay(100);
-} else if (input == "0329"){
+  tooth.print("Enter your password:");
+
+  if (tooth.available() > 0) {  //if something is inputed on device
+    input = tooth.read();
+    tooth.print("Password: ");
+    tooth.println(input);
+  }
+
+  if (input != "0329") {
+    digitalWrite(BUZZER, HIGH);
+    delay(1000);
+    digitalWrite(BUZZER, LOW);
+    delay(100);
+  } else if (input == "0329") {
     Serial.println("Password correct!");
     digitalWrite(GREEN_LED, HIGH);
     delay(1000);
     digitalWrite(GREEN_LED, LOW);
     delay(1000);
-    
   }
 
-for (int i = 0; i < 5; i++) { //beeps if the confirmation code was incorrect 
-  digitalWrite(BUZZER, HIGH);
-  delay(200);
-  digitalWrite(BUZZER, LOW);
-  delay(200);
+  for (int i = 0; i < 5; i++) {  //beeps if the confirmation code was incorrect
+    digitalWrite(BUZZER, HIGH);
+    delay(200);
+    digitalWrite(BUZZER, LOW);
+    delay(200);
   }
 
 
 
- // Reset the loop if no new card present on the sensor/reader. This saves the entire process when idle.
+  // Reset the loop if no new card present on the sensor/reader. This saves the entire process when idle.
 
-if ( ! mfrc522.PICC_IsNewCardPresent()) {
+  if (!mfrc522.PICC_IsNewCardPresent()) {
 
-return;
-
-}
-
+    return;
+  }
 
 
- // Select one of the cards
 
-if ( ! mfrc522.PICC_ReadCardSerial()) {
+  // Select one of the cards
 
-return;
+  if (!mfrc522.PICC_ReadCardSerial()) {
 
-}
+    return;
+  }
 
 
 
 
-MFRC522::PICC_Type piccType = mfrc522.PICC_GetType(mfrc522.uid.sak);
+  MFRC522::PICC_Type piccType = mfrc522.PICC_GetType(mfrc522.uid.sak);
 
 
 
-Serial.print(F("RFID Tag UID:"));
+  Serial.print(F("RFID Tag UID:"));
 
-printHex(mfrc522.uid.uidByte, mfrc522.uid.size);
+  printHex(mfrc522.uid.uidByte, mfrc522.uid.size);
 
-Serial.println("");
+  Serial.println("");
 
 
 
-mfrc522.PICC_HaltA(); // Halt PICC
-
+  mfrc522.PICC_HaltA();  // Halt PICC
 }
 
 void printHex(byte *buffer, byte bufferSize) {
 
 
 
- //Serial.begin("reading?");
+  //Serial.begin("reading?");
 
-for (byte i = 0; i < bufferSize; i++) {
+  for (byte i = 0; i < bufferSize; i++) {
 
-Serial.print(buffer[i] < 0x10 ? " 0" : " ");
+    Serial.print(buffer[i] < 0x10 ? " 0" : " ");
 
-Serial.print(buffer[i], HEX);
-
+    Serial.print(buffer[i], HEX);
+  }
 }
 
 
-}
